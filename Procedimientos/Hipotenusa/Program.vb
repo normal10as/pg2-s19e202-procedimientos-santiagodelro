092b0dfcc124eb6ab433e5c5f27c267ad9d37e46
Usuario <Usuario@NENONCHI>
2019-05-19 22:56:29 -0300

    2.3

---
+++ Procedimientos/Hipotenusa/Program.vb
@@ -0,0 +1,24 @@
+Imports System
+Imports System.Math
+Module Hipótenusa
+    Sub Main(args As String())
+        Console.WriteLine(hipotenusa())
+    End Sub
+    Function hipotenusa() As Integer
+        Dim a, b, c As Integer
+        Console.Write("Ingrese el lado A: ")
+        a = Console.ReadLine()
+        Console.Write("Ingrese el lado B: ")
+        b = Console.ReadLine()
+        c = Pow(a, 2) + Pow(b, 2)
+        c = Pow(c, 2)
+        Return ("La hipotenusa el triangulo es: " & c)
+    End Function
+
+End Module
+
+'Crear un proyecto y un módulo llamado “Hipotenusa” que implemente una función que
+'reciba el tamaño de los lados A y B, calcule y devuelva C (hipotenusa). Dados los lados A y
+'B de un triángulo rectángulo, según el teorema de Pitágoras, el cuadrado de la hipotenusa
+'(C), es igual a la suma del cuadrado de los catetos (lados) de lo que se da la siguiente
+'fórmula: c² = a² + b². Ingresar los valores por teclado y mostrar el resultado en consola.
